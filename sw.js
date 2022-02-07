dcb9bc6fd0aca914c98fa71bae2f7662ca34666d
sito2286 <47269232+sito2286@users.noreply.github.com>
2022-02-07 14:05:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
-    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    // 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
     'css/animate.css',
     'js/libs/jquery.js'
 ];
